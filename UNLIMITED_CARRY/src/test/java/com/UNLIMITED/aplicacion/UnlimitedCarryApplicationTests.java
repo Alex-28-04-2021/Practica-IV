442469b470e15771401a983ee4c8a467868dd241
Alex-28-04-2021 <epalaciosp@ucentral.edu.co>
2024-02-27 12:07:44 -0500

    commit inicial

---
+++ UNLIMITED_CARRY/src/test/java/com/UNLIMITED/aplicacion/UnlimitedCarryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UNLIMITED.aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlimitedCarryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
